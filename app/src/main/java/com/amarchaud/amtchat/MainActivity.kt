1506a43649e2b3793964335c9c4feccaa98d4fd6
Antoine Marchaud <tonio2835@gmail.com>
2020-12-16 21:36:57 +0100

    add MainViewModel (empty)

--- app/src/main/java/com/amarchaud/amtchat/MainActivity.kt
+++ app/src/main/java/com/amarchaud/amtchat/MainActivity.kt
@@ -2,11 +2,14 @@ package com.amarchaud.amtchat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.amarchaud.amtchat.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,30 +21,17 @@ class MainActivity : AppCompatActivity() {
         const val TAG = "MainActivity"
     }
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // nav host
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.my_first_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-
-        //appBarConfiguration = AppBarConfiguration(navController.graph)
-        // top Fragment (no arrow displayed, and display hamburger if there is a drawerlayout)
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.splashFragment,
-                R.id.createAccountFragment,
-                R.id.lastMessagesFragment
-            )
-        )
-
-        // actionBar config
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        // Toolbar config
-        //NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration)
+        viewModel =
+            ViewModelProvider(this).get(MainViewModel::class.java)
+        binding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/com/amarchaud/amtchat/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.amarchaud.amtchat
+
+import android.app.Application
+import com.amarchaud.amtchat.base.BaseViewModel
+
+class MainViewModel(app: Application) : BaseViewModel(app) {
+
+
+}
